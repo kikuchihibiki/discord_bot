7eaa1982cc5af6c5d3a89260673d6711e64ee227
kikuchihibiki <113155569+kikuchihibiki@users.noreply.github.com>
2024-04-16 14:45:33 +0900

    修正

--- bot.py
+++ bot.py
@@ -1,5 +1,4 @@
 import discord
-import config
 import random
 from discord.ext import commands
 from discord import app_commands
@@ -7,6 +6,7 @@ from discord.utils import get
 from io import StringIO
 from keep_alive import keep_alive
 from datetime import datetime, timedelta
+import os
 
 intents = discord.Intents.default()
 client = discord.Client(intents=intents)
@@ -26,6 +26,6 @@ async def roll_dice(interaction: discord.Interaction, sides: int = 6):
         return
     result = random.randint(1, sides)
     await interaction.response.send_message(f'サイコロを振った結果: {result}')
-
+TOKEN = os.getenv("DISCORD_TOKEN")
 keep_alive()
-client.run(config.DISCORD_TOKEN)
+client.run(TOKEN)
--- config.py
+++
@@ -1,5 +0,0 @@
-from dotenv import load_dotenv
-load_dotenv()
-
-import os
-DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
