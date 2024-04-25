991ee463d3533b4a8aec4ee721a164ba3a7a3660
kikuchihibiki <113155569+kikuchihibiki@users.noreply.github.com>
2024-04-25 14:50:11 +0900

    楽曲検索機能の追加

--- bot.py
+++ bot.py
@@ -7,6 +7,7 @@ from io import StringIO
 from keep_alive import keep_alive
 from datetime import datetime, timedelta
 import os
+from test_spotyfy import search_sound
 
 intents = discord.Intents.default()
 client = discord.Client(intents=intents)
@@ -26,6 +27,28 @@ async def roll_dice(interaction: discord.Interaction, sides: int = 6):
         return
     result = random.randint(1, sides)
     await interaction.response.send_message(f'サイコロを振った結果: {result}')
+
+@tree.command(name='song', description='search song')
+async def search_song(interaction: discord.Interaction, sides: str = "", artist_name: str = ""):
+    if not sides or not artist_name:
+        await interaction.response.send_message("曲名とアーティスト名を入力してください")
+        return
+    
+    results = search_sound(sides, artist_name)
+    if not results:
+        await interaction.response.send_message("曲が見つかりませんでした")
+        return
+    
+    embed = discord.Embed(title="Song Information", color=0x00ff00)
+    for result in results:
+        song_name, artist_name, preview_url, album_image_url = result
+        embed.add_field(name="Song Name", value=song_name, inline=False)
+        embed.add_field(name="Artist Name", value=artist_name, inline=False)
+        embed.add_field(name="Preview URL", value=preview_url, inline=False)
+        embed.set_image(url=album_image_url)
+
+    # ループが終了した後に1度だけメッセージを送信
+    await interaction.response.send_message(embed=embed)
 TOKEN = os.getenv("DISCORD_TOKEN")
 keep_alive()
 client.run(TOKEN)
---
+++ test_spotyfy.py
@@ -0,0 +1,39 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import os
+client_id = os.getenv("client_id")
+client_secret = os.getenv("client_secret")
+
+ccm = SpotifyClientCredentials(client_id = client_id, client_secret = client_secret)
+spotify = spotipy.Spotify(client_credentials_manager = ccm)
+
+def search_sound(song_name, artist_name):
+    if not song_name and not artist_name:
+        print("曲名またはアーティスト名を指定してください。")
+        return [{"error": "曲名またはアーティスト名を指定してください。"}]
+
+    if not song_name:
+        query = f"artist:{artist_name}"
+    elif not artist_name:
+        query = f"track:{song_name}"
+    else:
+        query = f"track:{song_name} artist:{artist_name}"
+
+    results = spotify.search(q=query, limit=1, type="track", market="JP", offset=0)
+    song_list = []
+    for track in results["tracks"]["items"]:
+        song_name = track["name"]  # 曲名
+        artist_name = ", ".join([artist["name"] for artist in track["artists"]])  # アーティスト名をリストから取得してカンマで連結
+        album_name = track["album"]["name"]  # アルバム名
+        release_date = track["album"]["release_date"]  # リリース日
+        popularity = track["popularity"]  # 人気度
+        duration_ms = track["duration_ms"]  # 曲の長さ(ms)
+        track_id = track["id"]  # トラックID
+        preview_url = track["preview_url"]  # プレビューURL
+        album_image = track["album"]["images"][0]["url"]  # アルバム画像URL
+        print(f"曲名{song_name}, アーティスト{artist_name}, アルバム名{album_name}, リリース日{release_date}, 人気度{popularity}, 曲の長さ{duration_ms}, トラックID{track_id}, URL{preview_url}, アルバム画像{album_image}")
+        song_list.append((song_name, artist_name, preview_url, album_image))
+    return song_list
+
+
+
